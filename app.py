b7ed92baef3327b626fcae632dc2a1d4c6c16166
zVitorSantos <vitorsantos4736@gmail.com>
2023-11-06 16:12:05 -0300

    Commit bug de carregar usuarios so tristeza

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, session, make_response
-from flask_login import UserMixin, login_required, current_user
+from flask import Flask, render_template, request, redirect, url_for, flash, session, make_response, jsonify
+from flask_login import UserMixin, LoginManager, AnonymousUserMixin, login_required, current_user
 from flask_sqlalchemy import SQLAlchemy
 from functools import wraps
 from datetime import timedelta
+import logging
 from werkzeug.security import generate_password_hash, check_password_hash
 import os
 
@@ -11,9 +12,16 @@ app.secret_key = '1234'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.abspath('database/users.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return db.session.get(User, int(user_id))
+
 db = SQLAlchemy(app)
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
@@ -24,6 +32,11 @@ class User(db.Model):
 
     def __repr__(self):
         return f'<User {self.username}>'
+    
+class Anonymous(AnonymousUserMixin):
+    is_admin = False
+
+login_manager.anonymous_user = Anonymous
 
 def get_user_by_email_or_username(email_or_username):
     user = User.query.filter((User.username == email_or_username) | (User.email == email_or_username)).first()
@@ -39,10 +52,40 @@ def check_credentials(username_or_email, password):
         return True
     return False
 
-def register_user(name, username, email, password):
+logging.basicConfig(level=logging.DEBUG)
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        logging.debug(f"Sessão atual: {session}")
+        if not current_user.is_authenticated:
+            logging.warning("Acesso sem autenticação")
+            return jsonify({'error': 'Não autenticado'}), 401
+        return f(*args, **kwargs)
+    return decorated_function
+
+def admin_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        logging.debug(f"Usuário {current_user.username} admin status: {current_user.is_admin}")
+        if not current_user.is_admin:
+            logging.warning("Acesso sem permissão de administrador")
+            return jsonify({'error': 'Acesso negado'}), 403
+        return f(*args, **kwargs)
+    return decorated_function
+
+@app.route('/check-permission', methods=['GET'])
+@login_required
+def check_permission():
+    if current_user.is_admin:
+        return jsonify({'isAllowed': True})
+    else:
+        return jsonify({'isAllowed': False}), 403
+
+def register_user(name, username, email, password, is_admin=False):
     try:
         hashed_password = generate_password_hash(password)
-        user = User(name=name, username=username, email=email, password=hashed_password)
+        user = User(name=name, username=username, email=email, password=hashed_password, is_admin=is_admin)
         db.session.add(user)
         db.session.commit()
         return user.id
@@ -50,27 +93,58 @@ def register_user(name, username, email, password):
         db.session.rollback()
         return None
 
-# Função decoradora para verificar se o usuário está logado
-def login_required(f):
-    @wraps(f)
-    def decorated_function(*args, **kwargs):
-        if 'user_id' not in session:
-            return redirect(url_for('login'))
-        return f(*args, **kwargs)
-    return decorated_function
+# Rota para listar usuários
+@app.route('/users')
+@login_required
+@admin_required 
+def list_users_route():
+    try:
+        users = User.query.all()
+        users_data = [
+            {
+                'id': user.id,
+                'name': user.name,
+                'username': user.username,
+                'email': user.email,
+                'is_admin': user.is_admin
+            } for user in users
+        ]
+        return jsonify(users_data)
+    except Exception as e:
+        logging.error(f'Erro ao buscar usuários: {e}')
+        return jsonify({'error': 'Erro interno do servidor'}), 500
 
-# Decorador para verificar se é admin
-def admin_required(f):
-    @wraps(f)
-    def decorated_function(*args, **kwargs):
-        if not current_user.is_admin: 
-            flash('Você não tem permissão para acessar esta página.', 'warning')
-            return redirect(url_for('main'))
-        return f(*args, **kwargs)
-    return decorated_function
+@app.route('/register', methods=['POST'])
+@login_required
+@admin_required
+def register_user_route():
+    data = request.form
+    name = data.get('name')
+    username = data.get('username')
+    email = data.get('email')
+    password = data.get('password')
+    is_admin = data.get('is_admin') == 'on'
+
+    # Verifique se o usuário ou email já existe
+    existing_user = User.query.filter((User.username == username) | (User.email == email)).first()
+    if existing_user:
+        return jsonify({'success': False, 'message': 'Nome de usuário ou email já cadastrado.'}), 409
+
+    # Caso não exista, prossiga com a criação do novo usuário
+    try:
+        hashed_password = generate_password_hash(password)
+        user = User(name=name, username=username, email=email, password=hashed_password, is_admin=is_admin)
+        db.session.add(user)
+        db.session.commit()
+        return jsonify({'success': True, 'user_id': user.id})
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({'success': False, 'message': str(e)}), 500
 
 @app.route('/')
 def main():
+    logging.debug(f"Sessão atual na rota principal: {session}")
+    logging.debug(f"Usuário atual na rota principal: {current_user}")
     if 'user_id' in session:
         # Busca o usuário pelo ID armazenado na sessão
         user = get_user_by_id(session['user_id'])
@@ -100,7 +174,7 @@ def login():
     if request.method == 'POST':
         email_or_username = request.form.get('loginName')
         password = request.form.get('loginPassword')
-        remember = 'loginCheck' in request.form  # Verifica se o checkbox "Me lembre" foi marcado
+        remember = 'loginCheck' in request.form 
 
         user = get_user_by_email_or_username(email_or_username)
         if user is None:
@@ -110,13 +184,16 @@ def login():
         if check_credentials(email_or_username, password):
             # Armazena o ID do usuário na sessão
             session['user_id'] = user.id
+            logging.debug(f"Usuário {user.username} logado com sucesso. Sessão: {session}")
             
             # Se o checkbox "Me lembre" foi marcado, a sessão se torna permanente
             if remember:
                 session.permanent = True
                 app.permanent_session_lifetime = timedelta(days=7)
+                logging.debug("Sessão configurada como permanente.")
             else:
                 session.permanent = False
+            logging.debug(f"Sessão permanente: {session.permanent}")
 
             return redirect(url_for('main'))
         else:
@@ -125,6 +202,15 @@ def login():
     
     # Se for uma solicitação GET, mostre o formulário de login
     return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    # Remove o ID do usuário da sessão
+    session.pop('user_id', None)
+    # Mostra uma mensagem ao usuário
+    flash('Você foi desconectado com sucesso.', 'success')
+    # Redireciona para a página de login ou para a página principal
+    return redirect(url_for('login'))
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ static/js/admin.js
@@ -0,0 +1,148 @@
+$(document).ready(function() {
+    function checkUserPermission(callback) {
+        $.ajax({
+            url: '/check-permission',
+            type: 'GET',
+            xhrFields: {
+                withCredentials: true
+            },
+            success: function(response) {
+                // O servidor deve responder com { 'isAllowed': true } para um usuário autorizado
+                callback(response.isAllowed);
+            },
+            error: function(xhr) {
+                // Se houver algum erro (como o usuário não estar autenticado ou não ter permissão), trate aqui
+                if (xhr.status === 401) {
+                    // Não autenticado
+                    alert('Você não está autenticado. Por favor, faça o login.');
+                } else if (xhr.status === 403) {
+                    // Não autorizado
+                    alert('Você não tem permissão para acessar esta funcionalidade.');
+                } else {
+                    // Outro erro
+                    alert('Ocorreu um erro ao verificar as permissões: ' + xhr.statusText);
+                }
+            }
+        });
+    }
+
+    // Função para atualizar a tabela de usuários
+    function updateUsersTable() {
+        $.ajax({
+            url: '/users',
+            type: 'GET',
+            dataType: 'json',
+            xhrFields: {
+                withCredentials: true
+            },
+            success: function(response) {
+                console.log(response);
+                if (Array.isArray(response)) {
+                    var tbodyEl = $('table tbody');
+                    tbodyEl.empty();
+                    response.forEach(function(user) {
+                        tbodyEl.append(`
+                            <tr>
+                                <td>${user.id}</td>
+                                <td>${user.name}</td>
+                                <td>${user.username}</td>
+                                <td>${user.email}</td>
+                                <td>${user.is_admin ? 'Sim' : 'Não'}</td>
+                                <td><button class="btn btn-sm btn-outline-secondary">Atualizar</button></td>
+                            </tr>
+                        `);
+                    });
+                } else {
+                    console.error('A resposta não é um array:', response);
+                }
+            },
+            error: function(xhr, status, error) {
+                handleAjaxError(xhr, status, error);
+            }
+        });
+    }
+
+    // Função para tratar erros de AJAX
+    function handleAjaxError(xhr, status, error) {
+        if (xhr.status === 401) {
+            // Verifique se não está na página de login antes de redirecionar
+            if (window.location.pathname !== '/login') {
+                console.error('Não autenticado: redirecionando para o login.');
+                window.location.href = '/login';
+            }
+        } else if (xhr.status === 403) {
+            console.error('Acesso negado: redirecionando para a página principal.');
+            window.location.href = '/';
+        } else if (xhr.status === 500) {
+            console.error('Erro interno do servidor: contate o administrador.');
+        } else {
+            console.error('Erro na comunicação com o servidor: ' + xhr.statusText);
+        }
+    }
+
+    // Função para mostrar mensagens de erro
+    function showError(message) {
+        $('#error-message').text(message).show();
+    }
+
+    // Manipulador de eventos para o botão de registro de usuário
+    $('#register_button').on('click', function() {
+        var name = $('#userName').val().trim();
+        var username = $('#userUsername').val().trim();
+        var email = $('#userEmail').val().trim();
+        var password = $('#password').val();
+        var confirmPassword = $('#confirm_password').val();
+        var isAdmin = $('#isAdmin').is(':checked');
+
+        if(password !== confirmPassword) {
+            showError('As senhas não coincidem.');
+            return;
+        }
+
+        var formData = {
+            name: name,
+            username: username,
+            email: email,
+            password: password,
+            is_admin: isAdmin
+        };
+
+        $.ajax({
+            url: '/register',
+            type: 'POST',
+            contentType: 'application/x-www-form-urlencoded',
+            data: formData,
+            success: function(response) {
+                if(response.success) {
+                    checkUserPermission(function(isPermitted) {
+                        if (isPermitted) {
+                            updateUsersTable();
+                            $('#registerUserModal').modal('hide');
+                            alert('Usuário criado com sucesso!');
+                        } else {
+                            showError('Você não tem permissão para criar um usuário.');
+                        }
+                    });
+                } else {
+                    showError(response.message || 'Erro ao criar o usuário. Tente novamente.');
+                }
+            },
+            error: function(xhr) {
+                if(xhr.status === 409) {
+                    showError('Nome de usuário ou email já cadastrado.');
+                } else {
+                    showError('Erro na comunicação com o servidor: ' + xhr.statusText);
+                }
+            }
+        });
+    });
+
+    // Chama a função para atualizar a tabela se o usuário estiver autorizado
+    checkUserPermission(function(isPermitted) {
+        if (isPermitted) {
+            updateUsersTable();
+        } else {
+            showError('Você não tem permissão para visualizar esta tabela.');
+        }
+    });
+});
